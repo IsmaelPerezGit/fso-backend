206bc06becf1a122e6381cb1081b06d957311b2f
Ismael <IsmaelPerezEmail@gmail.com>
2019-09-09 14:24:45 -0700

    -Added routes for deleting a note and adding a note

--- index.js
+++ index.js
@@ -5,40 +5,51 @@ const app = express();
 let notes = [
     {
         id: 1,
-        content: "HTML is easy",
-        date: "2019-05-30T17:30:31.098Z",
+        content: 'HTML is easy',
+        date: '2019-05-30T17:30:31.098Z',
         important: true
     },
     {
         id: 2,
-        content: "Browser can execute only Javascript",
-        date: "2019-05-30T18:39:34.091Z",
+        content: 'Browser can execute only Javascript',
+        date: '2019-05-30T18:39:34.091Z',
         important: false
     },
     {
         id: 3,
-        content: "GET and POST are the most important methods of HTTP protocol",
-        date: "2019-05-30T19:20:14.298Z",
+        content: 'GET and POST are the most important methods of HTTP protocol',
+        date: '2019-05-30T19:20:14.298Z',
         important: true
     }
-]
+];
 
 app.get('/', (req, res) => {
-    res.send('<h1>Hello User</h1>')
-})
+    res.send('<h1>Hello API User</h1>');
+});
 
 app.get('/notes', (req, res) => {
-    res.json(notes)
-})
+    res.json(notes);
+});
 
 app.get('/notes/:id', (req, res) => {
     const id = +req.params.id;
-    const note = notes.find(note => note.id === id)
-    res.json(note)
-})
+    const note = notes.find(note => note.id === id);
 
+    note ? res.json(note) : res.status(404).end();
+});
 
+app.delete('/notes/:id', (req, res) => {
+    id = +req.params.id;
+    notes = notes.filter(note => note.id !== id);
+
+    res.status(204).end();
+});
+
+app.post('/notes', (req, res) => {
+    const note = req.body;
+    res.json(note);
+});
 
 const PORT = 3001;
 app.listen(PORT);
-console.log(`Server listening on port ${PORT}`)
+console.log(`Server listening on port ${PORT}`);
